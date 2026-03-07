1c11f3bea19f5b3d43341cd439bead79faf6f00b
i4nizer <iansandoval264@gmail.com>
2026-03-07 15:01:58 +0800

    Created reading controller.

---
+++ src/controllers/reading.controller.ts
@@ -0,0 +1,59 @@
+import { Reading } from "@/models/reading.model.js"
+import { PaginationSchema } from "@/schemas/pagination.schema.js"
+import { ReadingCreateSchema, ReadingQuerySchema, ReadingUpdateSchema } from "@/schemas/reading.schema.js"
+import { type RequestHandler } from "express"
+import { Op } from "sequelize"
+
+//
+
+const get: RequestHandler = async (req, res) => {
+    const { data, error, success } = PaginationSchema.and(ReadingQuerySchema).safeParse(req.query)
+    if (!success) return res.status(400).send(error.issues.at(0)?.message)
+
+    const keys = Object.keys(PaginationSchema.shape)
+    const entries = Object.entries(data).filter(([k, v]) => !keys.includes(k) && v != undefined)
+
+    const { alpha, omega, limit, offset } = data
+    const where: any = Object.fromEntries(entries)
+    const createdAt = { ...(alpha && { [Op.gte]: alpha }), ...(omega && { [Op.lte]: omega }) }
+    if (Object.keys(createdAt).length) where.createdAt = createdAt
+
+    const readings = await Reading.findAll({ where: data, raw: true, limit, offset })
+    res.send(readings)
+}
+
+const post: RequestHandler = async (req, res) => {
+    const { data, error, success } = ReadingCreateSchema.safeParse(req.body)
+    if (!success) return res.status(400).send(error.issues.at(0)?.message)
+
+    const reading = await Reading.create(data)
+    res.send(reading.dataValues)
+}
+
+const patch: RequestHandler = async (req, res) => {
+    const id = req.params.id as string
+    if (!id) return res.status(400).send("Reading id required.")
+
+    const { data, error, success } = ReadingUpdateSchema.safeParse(req.body)
+    if (!success) return res.status(400).send(error.issues.at(0)?.message)
+
+    const reading = await Reading.findByPk(id)
+    if (!reading) return res.status(404).send("Reading not found.")
+
+    await reading.update(data)
+    res.send(reading.dataValues)
+}
+
+const destroy: RequestHandler = async (req, res) => {
+    const id = req.params.id as string
+    if (!id) return res.status(400).send("Reading id required.")
+
+    const count = await Reading.destroy({ where: { id } })
+    if (count <= 0) return res.status(404).send("Reading not found.")
+
+    return res.status(204).send("Reading deleted successfully.")
+}
+
+//
+
+export default { get, post, patch, destroy }
