04e7eeda2ee3ee076d70af32eac96a657f6b7df9
i4nizer <iansandoval264@gmail.com>
2026-03-07 12:01:05 +0800

    Added database details.

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,6 +4,18 @@ const env = {
 	port: Number(process.env.PORT),
 	host: process.env.HOST || "",
 	protocol: process.env.PROTOCOL || "",
+	database: {
+		user: process.env.DATABASE_USER || "",
+		pass: process.env.DATABASE_PASS || "",
+		host: process.env.DATABASE_HOST || "",
+		port: Number(process.env.DATABASE_PORT),
+		name: process.env.DATABASE_NAME || "",
+		log: process.env.DATABASE_LOG === "true",
+		sync: process.env.DATABASE_SYNC === "true",
+		alter: process.env.DATABASE_ALTER === "true",
+		force: process.env.DATABASE_FORCE === "true",
+		dialect: process.env.DATABASE_DIALECT || "",
+	},
 }
 
 //
