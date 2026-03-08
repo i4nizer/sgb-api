3b2a95239195c3c2d4083bf85ca504ab158b24e0
i4nizer <iansandoval264@gmail.com>
2026-03-08 10:23:03 +0800

    Moved middlewares to main express instance.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import logMiddleware from "@/middlewares/log.middleware.js"
 import env from "@/config/env.config.js"
 import sequelizeBoot from "@/boot/sequelize.boot.js"
 import apiRoute from "@/routes/api.route.js"
@@ -8,6 +11,20 @@ import adminBoot from "@/boot/admin.boot.js"
 
 // --- Server
 const api = express()
+
+// --- Middlewares
+const origin = [
+    "http://localhost",
+    "ionic://localhost",
+    "http://localhost:3000",
+    "capacitor://localhost",
+    "https://smart-germination-box.vercel.app",
+]
+api.use(cors({ origin, credentials: true }))
+api.use(cookieParser())
+api.use(express.json())
+api.use(express.urlencoded({ extended: true }))
+api.use(logMiddleware.log)
 
 // --- Routes
 api.get("/", (_, res) => res.send("I am api only."))
--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,7 +1,4 @@
 import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import logMiddleware from "@/middlewares/log.middleware.js"
 import jwtMiddleware from "@/middlewares/jwt.middleware.js"
 import authRoute from "@/routes/auth.route.js"
 import userRoute from "@/routes/user.route.js"
@@ -12,20 +9,6 @@ import thresholdRoute from "@/routes/threshold.route.js"
 //
 
 const router = express.Router()
-
-const origin = [
-    "http://localhost",
-    "ionic://localhost",
-    "http://localhost:3000",
-    "capacitor://localhost",
-    "https://smart-germination-box.vercel.app",
-]
-router.use(cors({ origin, credentials: true }))
-
-router.use(cookieParser())
-router.use(express.json())
-router.use(express.urlencoded({ extended: true }))
-router.use(logMiddleware.log)
 router.use("/auth", authRoute.router)
 router.use("/user", jwtMiddleware.requireAuthToken, userRoute.router)
 router.use("/capture", jwtMiddleware.requireAuthToken, captureRoute.router)
