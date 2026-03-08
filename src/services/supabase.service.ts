64906207637b70bbdf2c6a51445756ce35ee21ce
i4nizer <iansandoval264@gmail.com>
2026-03-08 14:34:55 +0800

    Added connect function to my supabase service.

--- src/services/supabase.service.ts
+++ src/services/supabase.service.ts
@@ -3,8 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 
 //
 
+// --- Instance
 const supabase = createClient(env.supabase.url, env.supabase.key)
+
+const connect = async () => await supabase.rpc("now")
 
 //
 
-export default { supabase }
+export default { supabase, connect }
