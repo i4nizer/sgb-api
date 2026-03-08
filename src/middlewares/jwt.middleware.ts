2e00e4229891fbb0b20a3b3e6d6b2a86a63c4343
i4nizer <iansandoval264@gmail.com>
2026-03-08 09:55:23 +0800

    Modified jwt middleware to get token from cookies.

--- src/middlewares/jwt.middleware.ts
+++ src/middlewares/jwt.middleware.ts
@@ -6,7 +6,7 @@ import { type UserSafeSchema } from "@/schemas/user.schema.js";
 //
 
 const requireAuthToken: RequestHandler = async (req, res, next) => {
-    const token = (req.headers.authorization || "").split(" ").at(1)
+    const token = req.cookies?.token as string | undefined
     if (!token) return res.status(401).send("Authorization is required.")
 
     await Promise
