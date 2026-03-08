a08afa3ad5d4c3c78cab37f5cd37be6af0e52fe2
i4nizer <iansandoval264@gmail.com>
2026-03-08 10:30:04 +0800

    Implemented user role check on /api/user.

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -5,12 +5,13 @@ import userRoute from "@/routes/user.route.js"
 import captureRoute from "@/routes/capture.route.js"
 import readingRoute from "@/routes/reading.route.js"
 import thresholdRoute from "@/routes/threshold.route.js"
+import roleMiddleware from "@/middlewares/role.middleware.js"
 
 //
 
 const router = express.Router()
 router.use("/auth", authRoute.router)
-router.use("/user", jwtMiddleware.requireAuthToken, userRoute.router)
+router.use("/user", jwtMiddleware.requireAuthToken, roleMiddleware.requireUserRole("Admin"), userRoute.router)
 router.use("/capture", jwtMiddleware.requireAuthToken, captureRoute.router)
 router.use("/reading", jwtMiddleware.requireAuthToken, readingRoute.router)
 router.use("/threshold", jwtMiddleware.requireAuthToken, thresholdRoute.router)
