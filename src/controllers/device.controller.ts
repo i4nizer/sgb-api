e9329b88bb83ed1eede19fbf6eb61ae15adb64f7
i4nizer <iansandoval264@gmail.com>
2026-03-10 00:19:56 +0800

    Removed test fcm in device registry.

--- src/controllers/device.controller.ts
+++ src/controllers/device.controller.ts
@@ -1,7 +1,6 @@
 import { type RequestHandler } from "express"
 import { Device } from "@/models/device.model.js"
 import { DeviceCreateSchema } from "@/schemas/device.schema.js"
-import firebaseService from "@/services/firebase.service.js"
 
 //
 
@@ -11,11 +10,7 @@ const post: RequestHandler = async (req, res) => {
     const { data, error, success } = DeviceCreateSchema.safeParse(req.body)
     if (!success) return res.status(400).send(error.issues.at(0)?.message)
     
-    const defaults = { ...data, userId: Number(req.user.id) }
-    const [device, created] = await Device.findOrCreate({ where: { userId: req.user.id }, defaults })
-
-    if (created) await firebaseService.fcm.send({ token: data.token, notification: { title: "Welcome to SGB!" } })
-    await device.update(data)
+    const device = await Device.create({ ...data, userId: Number(req.user.id) })
     res.send(device.dataValues)
 }
 
