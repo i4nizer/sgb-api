7edb40863a460c11a570d97ef17b6a8a1a458ccd
i4nizer <iansandoval264@gmail.com>
2026-03-15 21:10:52 +0800

    Avoided duplicate on device token.

--- src/controllers/device.controller.ts
+++ src/controllers/device.controller.ts
@@ -10,7 +10,8 @@ const post: RequestHandler = async (req, res) => {
     const { data, error, success } = DeviceCreateSchema.safeParse(req.body)
     if (!success) return res.status(400).send(error.issues.at(0)?.message)
     
-    const device = await Device.create({ ...data, userId: Number(req.user.id) })
+    const defaults = { ...data, userId: Number(req.user.id) }
+    const [device] = await Device.findOrCreate({ defaults, where: { token: data.token, } })
     res.send(device.dataValues)
 }
 
